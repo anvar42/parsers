4f73f2ed26cd2b288179d6cd76f26d4e5d660f68
Anvar571 <abdurashidovanvar52@gmail.com>
2025-08-08 23:58:25 +0500

    fix: add null and changed object blaue

--- jsonParser/json-types.ts
+++ jsonParser/json-types.ts
@@ -1,13 +1,13 @@
-export type JObjectType = { type: "Object", value: Object };
+export type JObjectType = { type: "Object", value: { [key: string]: ASTNode } };
 export type JStringType = { type: "String", value: string };
 export type JNumberType = { type: "Number", value: number };
-export type JFalseType = { type: "False", value: boolean };
-export type JTrueType = { type: "True", value: boolean };
+export type JFalseType = { type: "Boolean", value: boolean };
+export type JNull = { type: "Null" };
 
 export type ASTNode = 
     | JObjectType
     | JStringType
     | JNumberType
-    | JTrueType
     | JFalseType
+    | JNull
 ;
